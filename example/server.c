698256ca0326d8c5a4a1a8396fa2bedaa94295c9
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2024-03-11 23:09:12 +0530

    Update server.c

--- example/server.c
+++ example/server.c
@@ -8,16 +8,15 @@
 // 6) visit the url: http://localhost:8080
 // 7) Enjoy using rohanasan!!!!!
 
-
-#include "../include/rohanasan.h"
+#include "./include/rohanasan.h"
 
 const char* myhandle(struct request req)
 {
     if (eql(req.method, "GET")) {
         if (eql(req.path, "/"))
-            return send_http_response(default_html_header, "<h1>Hello!</h1>");
-        if (eql(req.path, "/path"))
             return send_file(default_html_header, "./html/index.html");
+        if (eql(req.path, "/come_here"))
+            return send_http_response(default_html_header, "<h1>Hello! you came here!</h1>");
     } else {
         return send_404();
     }
@@ -25,6 +24,7 @@ const char* myhandle(struct request req)
 
 int main()
 {
+    printf("Listening at http://localhost:8080\n");
     init(8080);
     serve(myhandle);
 }
